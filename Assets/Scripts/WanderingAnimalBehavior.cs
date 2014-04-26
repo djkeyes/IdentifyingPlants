555e66f1d657512c244dc88c97c7c84bf8e4edb4
Daniel Keyes <danieljkeyes@gmail.com>
2014-04-25 20:00:30 -0400

    Crows, birds, and rustling leaves

---
+++ Assets/Scripts/WanderingAnimalBehavior.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+
+public class WanderingAnimalBehavior : MonoBehaviour {
+
+	public float r;
+	public float f;
+	private Vector3 initialTransform;
+
+	// Use this for initialization
+	void Start () {
+		this.initialTransform = this.transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		this.transform.position = this.initialTransform + new Vector3 (r*(float)Math.Cos(f*Time.time),0, r*(float)Math.Sin(f*Time.time));
+	}
+}
