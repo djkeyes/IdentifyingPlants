7d76726687331ce36db7a1b5398f9b6f857d4d09
Daniel Keyes <danieljkeyes@gmail.com>
2014-04-25 20:08:30 -0400

    and footsteps

---
+++ Assets/Scripts/FootfallBehavior.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FootfallBehavior : MonoBehaviour {
+
+	public AudioSource soundsource;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector3 directionVector = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+		soundsource.mute = directionVector.magnitude == 0;
+	}
+}
